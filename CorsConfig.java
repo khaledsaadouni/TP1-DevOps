094fa772d00f67867dd99560679cc60491fd33fd
Aloui-Soumaya <soulayaaloui@gmail.com>
2024-02-08 17:17:22 +0100

    changes

--- CorsConfig.java
+++ CorsConfig.java
@@ -11,8 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Set the URL pattern of your API.addMapping("/api/**")  // Set the URL pattern of your API
                 .allowedHeaders("*")
-//                .allowedOrigins("http://localhost:3000")  // Allow requests from this origin
-//                .allowedOrigins("http://vps-8cbd5804.vps.ovh.net")  // Allow requests from this origin
+               .allowedOrigins("http://localhost:3000")  // Allow requests from this origin
+                .allowedOrigins("http://vps-8cbd5804.vps.ovh.net")  // Allow requests from this origin
                 .allowedOrigins("*")  // Allow requests from this origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow these HTTP methods
                 .allowCredentials(false)// Allow sending cookies from the client
